01ebfca4ac200fa424c4a1773da07f1fba3d3a22
Zheng Zhuang <1525019749@qq.com>
2023-03-08 23:53:20 +0000

    Implemented all functions of Manager.

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,5 @@
 from myapp import views
-from django.urls import path
+from django.urls import path,re_path
 
 app_name = 'myapp'
 
@@ -9,4 +9,10 @@ urlpatterns = [
     path('manager_login/', views.manager_login, name='manager_login'),
     path('administrator_home/', views.administrator_home, name='administrator_home'),
     path('manager_home/', views.manager_home, name='manager_home'),
+    path('personal_information/', views.personal_information, name='personal_information'),
+    path('warehouse/', views.warehouse, name='warehouse'),
+    path('record/', views.record, name='record'),
+    path('add_admin/', views.add_admin, name='add_admin'),
+    path('edit_admin/', views.edit_admin, name='edit_admin'),
+    path('delete_admin/', views.delete_admin, name='delete_admin'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib import messages
 from myapp import models
+import pymysql
+import tkinter.messagebox
+from tkinter import *
 
-# Create your views here.
+# 网站默认界面
 def index(request):
     context_dict = {}
     context_dict['boldmessage'] = '网站默认界面，限制酒厂的信息和两种登陆方法的链接！'
 
     return render(request, 'myapp/index.html', context=context_dict)
 
+# 经理登陆
 def manager_login(request):
     if request.method == 'POST':
         manager_name = request.POST.get('manager_name')
         manager_password = request.POST.get('manager_password')
-
-        # if administrator_name == 'zheng' and administrator_password == '123456':
-        #     return redirect('myapp:administrator_home')
-        # else:
-        #     return render(request, 'myapp/index.html')
 
         if manager_name and manager_password:
             name = manager_name.strip()
@@ -30,15 +29,11 @@ def manager_login(request):
 
     return render(request, 'myapp/manager_login.html')
 
+# admin登陆
 def administrator_login(request):
     if request.method == 'POST':
         administrator_name = request.POST.get('administrator_name')
         administrator_password = request.POST.get('administrator_password')
-
-        # if administrator_name == 'zheng' and administrator_password == '123456':
-        #     return redirect('myapp:administrator_home')
-        # else:
-        #     return render(request, 'myapp/index.html')
 
         if administrator_name and administrator_password:
             name = administrator_name.strip()
@@ -51,8 +46,70 @@ def administrator_login(request):
 
     return render(request, 'myapp/administrator_login.html')
 
+# 经理主界面，直接显示所用admin的信息
 def manager_home(request):
-    return HttpResponse('manager home')
+    context_dict = {}
+    conn = pymysql.connect(host='localhost', user='root', password='12345678', db='drinks', charset='utf8')
+    with conn.cursor(cursor=pymysql.cursors.DictCursor) as cursor:
+        cursor.execute('SELECT admin_id,admin_name,admin_pw,admin_age,admin_email,admin_telephone FROM administrator')
+        administrators = cursor.fetchall()
+    context_dict['administrators'] = administrators
+    return render(request, 'myapp/manager_home.html', context_dict)
 
+# 添加admin账户
+def add_admin(request):
+    if request.method == 'GET':
+        return render(request, 'myapp/add_admin.html')
+    else:
+        admin_name = request.POST.get('admin_name', '')
+        admin_pw = request.POST.get('admin_pw', '')
+        manager_name = request.POST.get('manager_name')
+        conn = pymysql.connect(host="localhost", user="root", passwd="12345678", db="drinks", charset='utf8')
+        with conn.cursor(cursor=pymysql.cursors.DictCursor) as cursor:
+            cursor.execute("INSERT INTO administrator (admin_name,admin_pw,manager_name)"
+                           "values (%s,%s,%s)", [admin_name,admin_pw,manager_name])
+            conn.commit()
+    return redirect('../')
+
+# 修改admin账户
+def edit_admin(request):
+    if request.method == 'GET':
+        admin_id = request.GET.get('admin_id')
+        context_dict = {}
+        conn = pymysql.connect(host="localhost", user="root", passwd="12345678", db="drinks", charset='utf8')
+        with conn.cursor(cursor=pymysql.cursors.DictCursor) as cursor:
+            cursor.execute("SELECT admin_id,admin_name,admin_pw FROM administrator where admin_id =%s", [admin_id])
+            admin = cursor.fetchone()
+        context_dict['administrator'] = admin
+        return render(request, 'myapp/edit_admin.html', context_dict)
+    else:
+        admin_id = request.POST.get('admin_id')
+        admin_name = request.POST.get("admin_name")
+        admin_pw = request.POST.get('admin_pw', '')
+        conn = pymysql.connect(host="localhost", user="root", passwd="12345678", db="drinks", charset='utf8')
+        with conn.cursor(cursor=pymysql.cursors.DictCursor) as cursor:
+            cursor.execute("UPDATE administrator SET admin_name=%s,admin_pw=%s where admin_id =%s",
+                           [admin_name, admin_pw, admin_id])
+            conn.commit()
+        return redirect('../')
+
+def delete_admin(request):
+    admin_id = request.GET.get('admin_id')
+    conn = pymysql.connect(host="localhost", user="root", passwd="12345678", db="drinks", charset='utf8')
+    with conn.cursor(cursor=pymysql.cursors.DictCursor) as cursor:
+        cursor.execute("DELETE FROM administrator WHERE admin_id=%s",[admin_id])
+        conn.commit()
+    return redirect('../')
+
+# admin主界面
 def administrator_home(request):
-    return HttpResponse('administrator home')
+    return render(request, 'myapp/administrator_home.html')
+
+def personal_information(request):
+    return render(request, 'myapp/personal_information.html')
+
+def warehouse(request):
+    return render(request, 'myapp/warehouse.html')
+
+def record(request):
+    return render(request, 'myapp/record.html')
